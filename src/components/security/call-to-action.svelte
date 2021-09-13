76f31d38a3eb10753a21f5e845eb8d71c7a0b5a4
nisarhassan12 <syednisarhassan12@gmail.com>
2021-09-14 00:26:20 +0500

    create the security page.

---
+++ src/components/security/call-to-action.svelte
@@ -0,0 +1,38 @@
+<script>
+  import Section from "../section.svelte";
+
+  const actions = [
+    {
+      title: "Security Vulnerability Disclosure Policy",
+      description:
+        "We welcome feedback from security researchers and the general public to help improve our security.",
+      link: {
+        href: "/",
+        text: "View report process",
+      },
+    },
+    {
+      title: "Thanks",
+      description:
+        "Big thanks the following people who responsibly disclosed their security findings.",
+      link: {
+        href: "/",
+        text: "View contributors",
+      },
+    },
+  ];
+</script>
+
+<Section>
+  <div class="flex justify-between max-w-3xl mx-auto">
+    {#each actions as a}
+      <div class="max-w-xs">
+        <h2 class="h3">{a.title}</h2>
+        <p class="my-x-small">
+          {a.description}
+        </p>
+        <a href={a.link.href} class="btn-primary">{a.link.text}</a>
+      </div>
+    {/each}
+  </div>
+</Section>
---
+++ src/components/security/report.svelte
@@ -0,0 +1,16 @@
+<script>
+  import Section from "../section.svelte";
+</script>
+
+<Section>
+  <div
+    class="bg-white shadow-normal rounded-4xl text-center p-large max-w-xl mx-auto"
+  >
+    <h2 class="h3">Report security concerns</h2>
+    <p class="my-x-small">
+      We welcome close collaboration with the worldwide security research
+      community.
+    </p>
+    <a href="/" class="btn-secondary">Report an incident</a>
+  </div>
+</Section>
