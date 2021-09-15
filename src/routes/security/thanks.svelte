5c04910c342ebb3c57322db48afcf71117ec5d9f
nisarhassan12 <syednisarhassan12@gmail.com>
2021-09-15 16:06:37 +0500

    merge the report page form Pr #937 and add the new Thanks widget to it.

--- src/routes/security/thanks.svelte
+++ src/routes/security/thanks.svelte
@@ -1,52 +1,9 @@
 <script>
  import Report from "../../components/security/report.svelte";
-
+ import Thanks from "../../components/security/thanks.svelte";
 </script>
 
-<style type="text/postcss">
-  .year {
-    display: inline-block;
-    border-bottom: 3px;
-    @apply border-orange-900 border-solid;
-  }
-
-  ol {
-    max-width: 285px;
-  }
-</style>
-
 <header>
-  <div class="max-w-lg mx-auto relative">
-    <img
-      src="/svg/security/heart.svg"
-      alt="Heart"
-      class="absolute -top-16 left-1/2 transform -translate-x-1/2 h-10"
-    />
-    <h1>Thanks</h1>
-    <p>
-      Big thanks the following people who responsibly disclosed their security
-      findings.
-    </p>
-  </div>
-
-  <div class="mt-x-large">
-    <h2 class="h4 year">2021</h2>
-    <ol class="text-left mx-auto my-x-small">
-      <li>
-        <a rel="nofollow" href="https://twitter.com/snoopysecurity"
-          >Sam Sanoop</a
-        > from Snyk.
-      </li>
-      <li>
-        <a
-          rel="nofollow"
-          href="https://about.gitlab.com/blog/2021/07/08/two-bugs-and-a-quick-fix-in-gitpod/"
-          >Joern Schneeweisz</a
-        > from GitLab.
-      </li>
-    </ol>
-  </div>
+  <Thanks />
 </header>
-
-
 <Report />
